8446d01b58ef072d53d950951cc4c6037278e912
Maxim Grankin <maximgran@gmail.com>
2022-05-23 01:31:03 +0400

    add cfg tests

--- compiler/fir/analysis-tests/testData/resolve/cfg/selfTypes.kt
+++ compiler/fir/analysis-tests/testData/resolve/cfg/selfTypes.kt
@@ -7,10 +7,31 @@ class JustSelfAnnotation {
 
 @Self
 class ReturnType {
-
-    fun returnType(): Self {
+    fun returnTypeWithVal(): Self {
         val res: Self  = this <!UNCHECKED_CAST!>as Self<!>
         return res
     }
+}
 
+@Self
+class ReturnTypeWithTypeParameter<T> {
+    fun returnType(): Self {
+        return this <!UNCHECKED_CAST!>as Self<!>
+    }
+}
+
+@Self
+class ReturnTypeWithTypeParameterы<T, A, F> {
+    fun returnType(): Self {
+        return this <!UNCHECKED_CAST!>as Self<!>
+    }
+}
+
+class InnerClass {
+    @Self
+    class Inner {
+        fun returnType(): Self {
+            return this <!UNCHECKED_CAST!>as Self<!>
+        }
+    }
 }
